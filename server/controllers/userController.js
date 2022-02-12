54c2d8810e81af8dd3e5c4174bdffb29a73e1c21
KHTRE <KHTREE@gmail.com>
2022-02-12 14:47:09 +0200

    bsckend is working

---
+++ server/controllers/userController.js
@@ -0,0 +1,71 @@
+const ApiError = require('../error/ApiError');
+const bcript = require('bcrypt');
+const jwt = require('jsonwebtoken'); //jsonwebtoken
+const { User, Basket} = require('../models/models');
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign(
+    {
+      id,
+      email,
+      role
+    },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: '24h'
+    }
+  );
+}
+
+class UserController {
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest('no email or password'));
+    }   
+    
+    const candidate = await User.findOne({
+      where: {email}
+    });
+
+    if (candidate) {
+      return next(ApiError.badRequest('email is already used'));
+    }
+
+    const hashPassword = await bcript.hash(password, 5);
+    const user = await User.create({
+      email,
+      role,
+      password: hashPassword
+    });
+    const basket = await Basket.create({userId: user.id});
+
+    const token = generateJwt(user.id, user.email, user.role);
+    
+    return res.json({token});
+  }
+
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({where: {email}});
+    if (!user) {
+      return next(ApiError.internal('no user with such email'));
+    }
+
+    let comparePassword = bcript.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal('wrong password!!!'));
+    }
+    const token = generateJwt(user.id, user.password, user.role);
+
+    return res.json({token});
+  }
+
+  async check(req, res, next) {
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({token});
+  }
+
+}
+
+module.exports = new UserController();
